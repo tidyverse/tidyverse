e1e742189b0f3be4cbb5b73d9945428eeae8cf67
hadley <h.wickham@gmail.com>
2017-01-19 17:25:34 -0600

    Add dummy imports and package docs

---
+++ R/tidyverse.R
@@ -0,0 +1,19 @@
+#' @keywords internal
+"_PACKAGE"
+
+# Suppress R CMD check note
+#' @importFrom broom tidy
+#' @importFrom ggplot2 ggplot
+#' @importFrom haven read_sas
+#' @importFrom hms hms
+#' @importFrom httr GET
+#' @importFrom jsonlite fromJSON
+#' @importFrom lubridate ymd
+#' @importFrom modelr add_predictions
+#' @importFrom readr read_csv
+#' @importFrom readxl read_excel
+#' @importFrom rvest html_node
+#' @importFrom stringr str_c
+#' @importFrom tidyr spread
+#' @importFrom xml2 read_xml
+NULL
