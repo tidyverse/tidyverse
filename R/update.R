b1881a01edce6a3b386d15447e3cf4c2321d0b5f
PoGibas <povilasgibas@gmail.com>
2018-08-27 02:03:41 +0300

    Return version 0 for non-installed dependecy package
    If dependency package is not installed utils::packageVersion will return error. Check if package is in installed.packages first.

--- R/update.R
+++ R/update.R
@@ -52,8 +52,15 @@ tidyverse_deps <- function(recursive = FALSE) {
   )
   pkg_deps <- setdiff(pkg_deps, base_pkgs)
 
+  pkg_installed <- rownames(installed.packages())
   cran_version <- lapply(pkgs[pkg_deps, "Version"], base::package_version)
-  local_version <- lapply(pkg_deps, utils::packageVersion)
+  local_version <- lapply(pkg_deps, function(x) {
+    if (x %in% pkg_installed) {
+      utils::packageVersion(x)
+    } else {
+      0
+    }
+  })
 
   behind <- purrr::map2_lgl(cran_version, local_version, `>`)
 
