5971901f85939df97e840f07d7ff7d21b5f08676
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-02-14 12:54:10 -0800

    Import a function from dbplyr, reprex, and rlang
    Addresses a CRAN NOTE seen on some platforms:
    
        Namespaces in Imports field not imported from:
         ‘dbplyr’ ‘reprex’ ‘rlang’
         All declared Imports should be used.

--- R/tidyverse.R
+++ R/tidyverse.R
@@ -3,6 +3,7 @@
 
 # Suppress R CMD check note
 #' @importFrom broom tidy
+#' @importFrom dbplyr show_query
 #' @importFrom forcats fct_c
 #' @importFrom ggplot2 ggplot
 #' @importFrom haven read_sas
@@ -13,6 +14,8 @@
 #' @importFrom modelr add_predictions
 #' @importFrom readr read_csv
 #' @importFrom readxl read_excel
+#' @importFrom reprex reprex
+#' @importFrom rlang qq_show
 #' @importFrom rvest html_node
 #' @importFrom stringr str_c
 #' @importFrom tidyr spread
