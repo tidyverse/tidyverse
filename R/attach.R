a5c8d38734931aab024de5f0c40fc954a4841d13
Gábor Csárdi <csardi.gabor@gmail.com>
2019-02-25 17:37:18 +0000

    Attach packages from same lib they were loaded from
    Otherwise install.packages() test loading might run
    into trouble, if the package versions are different,
    and (in some way) incompatible.
    
    Closes #171.

--- R/attach.R
+++ R/attach.R
@@ -9,6 +9,15 @@ core_unloaded <- function() {
   core[!search %in% search()]
 }
 
+## Attach the package from the same package library it was
+## loaded from before. https://github.com/tidyverse/tidyverse/issues/171
+
+same_library <- function(pkg) {
+  loc <- if (pkg %in% loadedNamespaces()) dirname(getNamespaceInfo(pkg, "path"))
+  do.call(
+    "library",
+    list(pkg, lib.loc = loc, character.only = TRUE, warn.conflicts = FALSE))
+}
 
 tidyverse_attach <- function() {
   to_load <- core_unloaded()
@@ -38,7 +47,7 @@ tidyverse_attach <- function() {
   msg(paste(info, collapse = "\n"), startup = TRUE)
 
   suppressPackageStartupMessages(
-    lapply(to_load, library, character.only = TRUE, warn.conflicts = FALSE)
+    lapply(to_load, same_library)
   )
 
   invisible()
