729aa240724bb4f97bc1b3cea5134c1809338991
Michael Chirico <chiricom@google.com>
2025-03-25 16:04:38 -0700

    reorganize generally

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,17 @@
 .onAttach <- function(...) {
-  if (!is_loading_for_tests()) {
-    attached <- tidyverse_attach()
-    inform_startup(tidyverse_attach_message(attached))
+  if (is_loading_for_tests()) {
+    return(invisible())
   }
 
-  if (!is_attached("conflicted") && !is_loading_for_tests()) {
-    conflicts <- tidyverse_conflicts()
-    inform_startup(tidyverse_conflict_message(conflicts))
+  attached <- tidyverse_attach()
+  inform_startup(tidyverse_attach_message(attached))
+
+  if (is_attached("conflicted")) {
+    return(invisible())
   }
+
+  conflicts <- tidyverse_conflicts()
+  inform_startup(tidyverse_conflict_message(conflicts))
 }
 
 is_attached <- function(x) {
