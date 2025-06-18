1c6afa4f6b943480b405f30b9557d4726d776efe
Michael Chirico <chiricom@google.com>
2025-06-18 10:26:17 -0700

    Improve organization of .onAttach (#355)

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,17 @@
 .onAttach <- function(...) {
-  attached <- tidyverse_attach()
-  if (!is_loading_for_tests()) {
-    inform_startup(tidyverse_attach_message(attached))
+  if (is_loading_for_tests()) {
+    return(invisible())
   }
 
-  if (!is_attached("conflicted") && !is_loading_for_tests()) {
-    conflicts <- tidyverse_conflicts()
-    inform_startup(tidyverse_conflict_message(conflicts))
+  attached <- tidyverse_attach()
+  inform_startup(tidyverse_attach_message(attached))
+
+  if (is_attached("conflicted")) {
+    return(invisible())
   }
+
+  conflicts <- tidyverse_conflicts()
+  inform_startup(tidyverse_conflict_message(conflicts))
 }
 
 is_attached <- function(x) {
